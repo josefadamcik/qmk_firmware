7aeb8cdd58b3fce55329b390734b438a264a4c31
Josef Adamcik <josef.adamcik@gmail.com>
2019-01-06 12:16:08 +0100

    Add alternative default layer (qwerty) and allow switching.

--- keyboards/katana60/keymaps/josef/keymap.c
+++ keyboards/katana60/keymaps/josef/keymap.c
@@ -18,13 +18,25 @@
 #define _______ KC_TRNS
 #define XXXXXXX KC_NO
 
+enum katana_layers {
+  _COLEMAK,
+  _QWERTY, 
+  _NUMB,
+  _SYMB,
+  _EXT
+};
 
-#define K_SPCFN LT(SYMB, KC_SPACE) // Tap for space, hold for symbols layer
-#define K_BSPFN LT(SYMB, KC_BSPC)  // Tap for backspace, hold for symbols layer
+enum katana_keycodes {
+  COLEMAK = SAFE_RANGE,
+  QWERTY
+};
+
+#define K_SPCFN LT(_SYMB, KC_SPACE) // Tap for space, hold for symbols layer
+#define K_BSPFN LT(_SYMB, KC_BSPC)  // Tap for backspace, hold for symbols layer
 #define LEFT_MOD MT(MOD_RGUI, KC_LEFT)
 #define DOWN_MOD MT(MOD_RALT, KC_DOWN)
 #define UP_MOD MT(MOD_RCTL, KC_UP)
-//linux
+//linux/wsin
 // #define UNDO    LCTL(KC_Z)         // UNDO
 // #define CUT     LCTL(KC_X)         // CUT
 // #define COPY    LCTL(KC_C)         // COPY
@@ -43,47 +55,51 @@
 #define K_LSTRT LGUI(KC_LEFT)      // Start of line
 #define K_LEND LGUI(KC_RIGHT)      // End of line
 
-#define BASE 0 // Default
-#define NUMB 1 // Numbers
-#define SYMB 2 // Symbols
-#define CURS 3 // Text Editing
+
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-[BASE] = LAYOUT( /* Base */
-  KC_ESC,  KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    DF(1),   KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,
-  KC_TAB,  KC_Q,    KC_W,    KC_F,    KC_P,    KC_G,    KC_LBRC,          KC_RBRC, KC_J,    KC_L,    KC_U,    KC_Y,    KC_SCLN, KC_BSPC,
-  MO(3),   KC_A,    KC_R,    KC_S,    KC_T,    KC_D,    KC_MINS,          KC_QUOT, KC_H,    KC_N,    KC_E,    KC_I,    KC_O,    KC_ENT,
-  KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_EQL,  KC_DEL,  KC_BSLS, KC_K,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
-  MO(2),   KC_LCTL, KC_LALT, KC_LGUI,                   K_BSPFN, KC_ENT,  KC_SPACE,         LEFT_MOD,DOWN_MOD,UP_MOD,   KC_RIGHT,MO(2)
+[_COLEMAK] = LAYOUT(
+  KC_ESC,   KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,   TG(_NUMB),KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,
+  KC_TAB,   KC_Q,    KC_W,    KC_F,    KC_P,    KC_G,    KC_LBRC,          KC_RBRC, KC_J,    KC_L,    KC_U,    KC_Y,    KC_SCLN, KC_BSPC,
+  MO(_EXT), KC_A,    KC_R,    KC_S,    KC_T,    KC_D,    KC_MINS,          KC_QUOT, KC_H,    KC_N,    KC_E,    KC_I,    KC_O,    KC_ENT,
+  KC_LSFT,  KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_EQL,  KC_DEL,  KC_BSLS, KC_K,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
+  MO(_SYMB),KC_LCTL, KC_LALT, KC_LGUI,                   K_BSPFN, KC_ENT,  KC_SPACE,         LEFT_MOD,DOWN_MOD,UP_MOD,  KC_RIGHT,MO(_SYMB)
 ),
-[NUMB] = LAYOUT(
-  _______, _______, _______, _______, _______, _______, _______, DF(0),   KC_PSLS, KC_PAST, KC_PMNS, _______, _______, _______, _______,
+[_QWERTY] = LAYOUT(
+  KC_ESC,   KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,   TG(_NUMB),KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,
+  KC_TAB,   KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_LBRC,          KC_RBRC, KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
+  MO(_EXT), KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_MINS,          KC_QUOT, KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_ENT,
+  KC_LSFT,  KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_EQL,  KC_DEL,  KC_BSLS, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
+  MO(_SYMB),KC_LCTL, KC_LALT, KC_LGUI,                   K_BSPFN, KC_ENT,  KC_SPACE,         LEFT_MOD,DOWN_MOD,UP_MOD,   KC_RIGHT,MO(_SYMB)
+),
+[_NUMB] = LAYOUT(
+  _______, _______, _______, _______, _______, _______, _______, _______, KC_PSLS, KC_PAST, KC_PMNS, _______, _______, _______, _______,
   _______, _______, _______, KC_MS_U, _______, _______, _______,          KC_7,    KC_8,    KC_9,    KC_PPLS, _______, _______, _______,
   _______, KC_BTN2, KC_MS_L, KC_MS_D, KC_MS_R, _______, _______,          KC_4,    KC_5,    KC_6,    KC_PPLS, _______, _______, _______,
   _______, _______, KC_ACL0, KC_ACL1, KC_ACL2, _______, _______, _______, KC_1,    KC_2,    KC_3,    KC_PENT, _______, _______, _______,
   _______, _______, _______, _______,                   KC_BTN1, _______, KC_P0,            KC_PDOT, KC_PENT, _______, _______, _______
-    ),
-[SYMB] = LAYOUT(
+),
+[_SYMB] = LAYOUT(
   RESET,   _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   _______, KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,
   KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    _______,          _______, KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_DEL,
   _______, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_VOLD,          KC_VOLU, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, _______,
   _______, KC_PLUS, KC_MINS, KC_EQL,  KC_LCBR, KC_RCBR, KC_MPRV, KC_MPLY, KC_MNXT, KC_LBRC, KC_RBRC, KC_SCLN, KC_COLN, KC_BSLS, _______,
   _______, _______, _______, _______,                   KC_DEL,  _______, _______,          _______, _______, _______, _______, _______
-    ),
-[CURS] = LAYOUT(
-  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+),
+[_EXT] = LAYOUT(
+  RESET  , DF(_COLEMAK), DF(_QWERTY) , _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
   _______, _______, KC_WH_U, _______, _______, _______, _______,          _______, KC_PGUP, K_PRVWD, KC_UP,   K_NXTWD, _______, _______,
   _______, KC_LCTL, KC_WH_D, KC_LSFT, _______, KC_CAPS, _______,          _______, KC_PGDN, KC_LEFT, KC_DOWN, KC_RIGHT,KC_DEL,  _______,
   _______, UNDO,    CUT,     COPY,    PASTE,   _______, _______, _______, _______, _______, K_LSTRT, _______, K_LEND,  _______, _______,
   _______, _______, _______, _______,                   _______, _______, _______,          _______, _______, _______, _______, _______
-    )
+)
 
 };
 
 
 const uint16_t PROGMEM fn_actions[] = {
-    [0] = ACTION_LAYER_TAP_KEY(CURS, KC_BSPC),
-    [1] = ACTION_LAYER_TAP_KEY(SYMB, KC_SPACE),
+    [0] = ACTION_LAYER_TAP_KEY(_EXT, KC_BSPC),
+    [1] = ACTION_LAYER_TAP_KEY(_SYMB, KC_SPACE),
 };
 
 const macro_t *action_get_macro(keyrecord_t *record, uint8_t id, uint8_t opt)
