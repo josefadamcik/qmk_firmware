50ae5af68a736fef896e053da3af39c575db928b
Josef Adamcik <josef.adamcik@gmail.com>
2019-01-06 13:18:44 +0100

    Support for Mac versus Linux(Win) defualt/extended layers. Details in readme

--- keyboards/katana60/keymaps/josef/keymap.c
+++ keyboards/katana60/keymaps/josef/keymap.c
@@ -19,8 +19,10 @@
 #define XXXXXXX KC_NO
 
 enum katana_layers {
-  _COLEMAK,
-  _QWERTY, 
+  _M_COLEMAK, /* mac */
+  _M_QWERTY, 
+  _W_COLEMAK, /* win/linux */
+  _W_QWERTY,   
   _NUMB,
   _SYMB,
   _M_EXT, /* mac */
@@ -28,16 +30,18 @@ enum katana_layers {
 };
 
 enum katana_keycodes {
-  COLEMAK = SAFE_RANGE,
-  QWERTY
+  M_COLEMAK = SAFE_RANGE,
+  M_QWERTY,
+  W_COLEMAK,
+  W_QWERTY
 };
 
 #define K_SPCFN LT(_SYMB, KC_SPACE) // Tap for space, hold for symbols layer
 #define K_BSPFN LT(_SYMB, KC_BSPC)  // Tap for backspace, hold for symbols layer
-#define LEFT_MOD MT(MOD_RGUI, KC_LEFT)
-#define DOWN_MOD MT(MOD_RALT, KC_DOWN)
-#define UP_MOD MT(MOD_RCTL, KC_UP)
 //linux/win
+#define W_LEFT_MOD MT(MOD_RCTL, KC_LEFT)
+#define W_DOWN_MOD MT(MOD_RALT, KC_DOWN)
+#define W_UP_MOD MT(MOD_RGUI, KC_UP)
 #define W_UNDO    LCTL(KC_Z)         // UNDO
 #define W_CUT     LCTL(KC_X)         // CUT
 #define W_COPY    LCTL(KC_C)         // COPY
@@ -47,6 +51,9 @@ enum katana_keycodes {
 #define W_LSTRT KC_HOME         // Start of line
 #define W_LEND KC_END           // End of line
 //mac
+#define M_LEFT_MOD MT(MOD_RGUI, KC_LEFT)
+#define M_DOWN_MOD MT(MOD_RALT, KC_DOWN)
+#define M_UP_MOD MT(MOD_RCTL, KC_UP)
 #define M_UNDO LGUI(KC_Z)            // UNDO
 #define M_CUT LGUI(KC_X)             // CUT
 #define M_COPY LGUI(KC_C)            // COPY
@@ -59,19 +66,33 @@ enum katana_keycodes {
 
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-[_COLEMAK] = LAYOUT(
+[_M_COLEMAK] = LAYOUT(
   KC_ESC,    KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,   TG(_NUMB),KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,
   KC_TAB,    KC_Q,    KC_W,    KC_F,    KC_P,    KC_G,    KC_LBRC,          KC_RBRC, KC_J,    KC_L,    KC_U,    KC_Y,    KC_SCLN, KC_BSPC,
   MO(_M_EXT),KC_A,    KC_R,    KC_S,    KC_T,    KC_D,    KC_MINS,          KC_QUOT, KC_H,    KC_N,    KC_E,    KC_I,    KC_O,    KC_ENT,
   KC_LSFT,   KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_EQL,  KC_DEL,  KC_BSLS, KC_K,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
-  MO(_SYMB), KC_LCTL, KC_LALT, KC_LGUI,                   K_BSPFN, KC_ENT,  KC_SPACE,         LEFT_MOD,DOWN_MOD,UP_MOD,  KC_RIGHT,MO(_SYMB)
+  MO(_SYMB), KC_LCTL, KC_LALT, KC_LGUI,                   K_BSPFN, KC_ENT,  KC_SPACE,         M_LEFT_MOD,M_DOWN_MOD,M_UP_MOD,  KC_RIGHT,MO(_SYMB)
 ),
-[_QWERTY] = LAYOUT(
-  KC_ESC,   KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,   TG(_NUMB),KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,
-  KC_TAB,   KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_LBRC,          KC_RBRC, KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
-  MO(_M_EXT), KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_MINS,          KC_QUOT, KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_ENT,
-  KC_LSFT,  KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_EQL,  KC_DEL,  KC_BSLS, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
-  MO(_SYMB),KC_LCTL, KC_LALT, KC_LGUI,                   K_BSPFN, KC_ENT,  KC_SPACE,         LEFT_MOD,DOWN_MOD,UP_MOD,   KC_RIGHT,MO(_SYMB)
+[_M_QWERTY] = LAYOUT(
+  KC_ESC,    KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,   TG(_NUMB),KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,
+  KC_TAB,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_LBRC,          KC_RBRC, KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
+  MO(_M_EXT),KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_MINS,          KC_QUOT, KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_ENT,
+  KC_LSFT,   KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_EQL,  KC_DEL,  KC_BSLS, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
+  MO(_SYMB), KC_LCTL, KC_LALT, KC_LGUI,                   K_BSPFN, KC_ENT,  KC_SPACE,         M_LEFT_MOD,M_DOWN_MOD,M_UP_MOD,   KC_RIGHT,MO(_SYMB)
+),
+[_W_COLEMAK] = LAYOUT(
+  KC_ESC,    KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,   TG(_NUMB),KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,
+  KC_TAB,    KC_Q,    KC_W,    KC_F,    KC_P,    KC_G,    KC_LBRC,          KC_RBRC, KC_J,    KC_L,    KC_U,    KC_Y,    KC_SCLN, KC_BSPC,
+  MO(_W_EXT),KC_A,    KC_R,    KC_S,    KC_T,    KC_D,    KC_MINS,          KC_QUOT, KC_H,    KC_N,    KC_E,    KC_I,    KC_O,    KC_ENT,
+  KC_LSFT,   KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_EQL,  KC_DEL,  KC_BSLS, KC_K,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
+  MO(_SYMB), KC_LGUI, KC_LALT, KC_LCTL,                   K_BSPFN, KC_ENT,  KC_SPACE,         W_LEFT_MOD,M_DOWN_MOD,M_UP_MOD,  KC_RIGHT,MO(_SYMB)
+),
+[_W_QWERTY] = LAYOUT(
+  KC_ESC,    KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,   TG(_NUMB),KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,
+  KC_TAB,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_LBRC,          KC_RBRC, KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
+  MO(_W_EXT),KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_MINS,          KC_QUOT, KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_ENT,
+  KC_LSFT,   KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_EQL,  KC_DEL,  KC_BSLS, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
+  MO(_SYMB), KC_LGUI, KC_LALT, KC_LCTL,                   K_BSPFN, KC_ENT,  KC_SPACE,         M_LEFT_MOD,M_DOWN_MOD,M_UP_MOD,   KC_RIGHT,MO(_SYMB)
 ),
 [_NUMB] = LAYOUT(
   _______, _______, _______, _______, _______, _______, _______, _______, KC_PSLS, KC_PAST, KC_PMNS, _______, _______, _______, _______,
@@ -88,14 +109,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   _______, _______, _______, _______,                   KC_DEL,  _______, _______,          _______, _______, _______, _______, _______
 ),
 [_M_EXT] = LAYOUT(
-  RESET  , COLEMAK, QWERTY , _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+  RESET  ,M_COLEMAK,M_QWERTY,W_COLEMAK,W_QWERTY,_______,_______, _______, _______, _______, _______, _______, _______, _______, _______,
   _______, _______, KC_WH_U, _______, _______, _______, _______,          _______, KC_PGUP, M_PRVWD, KC_UP,   M_NXTWD, _______, _______,
   _______, KC_LCTL, KC_WH_D, KC_LSFT, _______, KC_CAPS, _______,          _______, KC_PGDN, KC_LEFT, KC_DOWN, KC_RIGHT,KC_DEL,  _______,
   _______, M_UNDO,  M_CUT,   M_COPY,  M_PASTE, _______, _______, _______, _______, _______, M_LSTRT, _______, M_LEND,  _______, _______,
   _______, _______, _______, _______,                   _______, _______, _______,          _______, _______, _______, _______, _______
 ),
 [_W_EXT] = LAYOUT(
-  RESET  , COLEMAK, QWERTY , _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+  RESET  ,M_COLEMAK,M_QWERTY,W_COLEMAK,W_QWERTY,_______,_______, _______, _______, _______, _______, _______, _______, _______, _______,
   _______, _______, KC_WH_U, _______, _______, _______, _______,          _______, KC_PGUP, W_PRVWD, KC_UP,   W_NXTWD, _______, _______,
   _______, KC_LCTL, KC_WH_D, KC_LSFT, _______, KC_CAPS, _______,          _______, KC_PGDN, KC_LEFT, KC_DOWN, KC_RIGHT,KC_DEL,  _______,
   _______, W_UNDO,  W_CUT,   W_COPY,  W_PASTE, _______, _______, _______, _______, _______, W_LSTRT, _______, W_LEND,  _______, _______,
@@ -114,15 +135,27 @@ void matrix_scan_user(void) {
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
   switch (keycode) {
-    case QWERTY:
+    case M_QWERTY:
       if (record->event.pressed) {
-        set_single_persistent_default_layer(_QWERTY);
+        set_single_persistent_default_layer(_M_QWERTY);
       }
       return false;
       break;
-    case COLEMAK:
+    case M_COLEMAK:
       if (record->event.pressed) {
-        set_single_persistent_default_layer(_COLEMAK);
+        set_single_persistent_default_layer(_M_COLEMAK);
+      }
+      return false;
+      break;
+    case W_QWERTY:
+      if (record->event.pressed) {
+        set_single_persistent_default_layer(_W_QWERTY);
+      }
+      return false;
+      break;
+    case W_COLEMAK:
+      if (record->event.pressed) {
+        set_single_persistent_default_layer(_W_COLEMAK);
       }
       return false;
       break;
--- keyboards/katana60/keymaps/josef/readme.md
+++ keyboards/katana60/keymaps/josef/readme.md
@@ -1,5 +1,13 @@
 # The colemak keymap for katana60
 
-Same as the default layout, but with default Colemal layout.
+Customized for Josef Adamcik with some fancy stuff.
 
-# TODO: references to extend layer and symbol layers
+- Arrows at the bottom right have dual functionality: tap -> arrow, hold -> modifier.
+- Supports 4 default layers: Colemak Mac, Qwerty mac, Colemak Win/Linux, Qwerty Win/Linux. Swiching between layers is persistent.
+
+Mac versus Win/Linux layers:
+
+- modifiers are ordered differently. Mac version has (from middle to outer) CMD, ALT, CTRL, win/linux version has CTRL, ALT, CMD. It's ment to make switching between platforms easier. I'll still require some keymap changes in various software, but it does help with the biggest problem where main modifier for shortcuts on linux is usually ctrl but on mac it's cmd.
+- Extend layer is different, so keys for "previous/next word" and "copy",   "paste", "cut", and "undo" work properly. 
+
+
