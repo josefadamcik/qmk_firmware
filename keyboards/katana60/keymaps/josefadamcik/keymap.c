c50651dd658f5fa24ba2f7b6ca1699c23cbd0415
Josef Adamcik <josef.adamcik@gmail.com>
2019-07-11 20:47:23 +0200

    Working on the Crkbd mode for katana60 - lower layer

--- keyboards/katana60/keymaps/josefadamcik/keymap.c
+++ keyboards/katana60/keymaps/josefadamcik/keymap.c
@@ -139,9 +139,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 ),
 [_LOWER] = LAYOUT(
     XXXXX,    XXXXX,   XXXXX,   XXXXX,   XXXXX,   XXXXX,    XXXXX,  XXXXX,  XXXXX, XXXXX,   XXXXX,   XXXXX,   XXXXX,    XXXXX,   XXXXX,
-    KC_ESC,    KC_Q,    KC_W,    KC_F,    KC_P,    KC_G,    XXXXX,          XXXXX,  KC_J,    KC_L,    KC_U,    KC_Y,    KC_SCLN, KC_BSPC,
-    KC_CTLTB  ,KC_A,    KC_R,    KC_S,    KC_T,    KC_D,    XXXXX,          XXXXX,  KC_H,    KC_N,    KC_E,    KC_I,    KC_O,    KC_ENT,
-    KC_LSFT,   KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    XXXXX,  XXXXX,  XXXXX,  KC_K,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
+    KC_F1,    KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,    XXXXX,          XXXXX, KC_F7,   KC_F8,   KC_F9,   KC_F10,   KC_F11, KC_F12,
+    KC_CTLTB  ,KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    XXXXX,          XXXXX,  KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_ENT,
+    KC_LSFT, KC_GRV,   XXXXX,   XXXXX, KC_MINS,  KC_EQL,    XXXXX,  XXXXX,  XXXXX,  KC_LBRC, KC_RBRC, KC_QUOT, XXXXX,iiKC_BSLS, KC_RSFT,
     XXXXX, XXXXX,      KC_LGUI, KC_LOWER,                  KC_ENT,  XXXXX,KC_SPACE,         KC_RAISE, KC_RALT, XXXXX,     XXXXX,   XXXXX
 ),
 [_RAISE] = LAYOUT(
